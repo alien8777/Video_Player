9954ab7308a55bec7339dcf14e75991b53efe787
Ayan Kumar Jana <live.softwarecod4k@gmail.com>
2022-12-10 23:33:28 +0530

    Navigation drawer implemented and each item click event handled

--- app/src/main/java/com/example/video_player/MainActivity.kt
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.video_player
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.video_player.databinding.ActivityMainBinding
 import fragments.FoldersFragment
 import fragments.VideoFragment
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +25,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.ak_pink_nav)
         setContentView(binding.root)
+
+        //for Navigation drawer
+        toggle =
+            ActionBarDrawerToggle(this@MainActivity, binding.root, R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         setFragment(VideoFragment())
-        requestFilePermission()
+        requestRunTimeFilePermission()
 
 
         binding.bottomNav.setOnItemSelectedListener {
@@ -31,6 +44,31 @@ class MainActivity : AppCompatActivity() {
                 R.id.foldersView -> setFragment(FoldersFragment())
             }
             return@setOnItemSelectedListener true
+        }
+
+        binding.navViewDrawer.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.feedBack -> Toast.makeText(
+                    this@MainActivity,
+                    "FeedBack Pressed",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.themes -> Toast.makeText(
+                    this@MainActivity,
+                    "Theme Pressed",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.sortOrder -> Toast.makeText(
+                    this@MainActivity,
+                    "SortOrder Pressed",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.about -> Toast.makeText(this@MainActivity, "About Pressed", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.exit -> exitProcess(1)
+            }
+
+            return@setNavigationItemSelectedListener true
         }
 
     }
@@ -43,7 +81,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //for requesting permission
-    private fun requestFilePermission(): Boolean {
+    private fun requestRunTimeFilePermission(): Boolean {
         if (ActivityCompat.checkSelfPermission(
                 this@MainActivity,
                 WRITE_EXTERNAL_STORAGE
@@ -66,10 +104,19 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 13) {
-            Toast.makeText(this@MainActivity, "Permission granted", Toast.LENGTH_SHORT).show()
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Permission granted", Toast.LENGTH_SHORT).show()
+            }
         } else {
             Toast.makeText(this@MainActivity, "Permission denied", Toast.LENGTH_SHORT).show()
-            requestFilePermission()
+            requestRunTimeFilePermission()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
